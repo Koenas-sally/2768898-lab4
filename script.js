e129e8d2102e39e5ad7f294bb5f9d2594cbbc97c
Makoena Mabotja <makoenasally62@gmail.com>
2026-03-02 14:54:55 +0200

    feat: Added DOM elemnts

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('country-input');
+const searchBtn = document.getElementById('search-btn');
+const spinner = document.getElementById('loading-spinner');
+const countryInfo = document.getElementById('country-info');
+const bordersSection = document.getElementById('bordering-countries');
+const errorMessage = document.getElementById('error-message');
+
